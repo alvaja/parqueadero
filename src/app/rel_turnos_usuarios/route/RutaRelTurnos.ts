cda24fbded031d3aed27c4b84efe51fb33f5c239
alvaja <alvaro-leiva@hotmail.com>
2025-03-23 11:09:02 -0500

    validarRelDatos creado

--- src/app/rel_turnos_usuarios/route/RutaRelTurnos.ts
+++ src/app/rel_turnos_usuarios/route/RutaRelTurnos.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import controladorRelTurnosCrear from "../controller/ControladorRelTurnosCrear";
 import validarDatos from "../../../middleware/ValidarDatos";
-import { datosTurnoCrear } from "../../../config/domain/ValidarTurno";
+import { datosRelTurnosActualizar, datosRelTurnosBorrar, datosRelTurnosCrear } from "../../../config/domain/ValidarRelTurnos";
 import controladorRelTurnosConsulta from "../controller/ControladorRelTurnosConsultar";
 import controladorRelTurnosBorrar from "../controller/ControladorRelTurnosBorrar";
 import controladorRelTurnosActualizar from "../controller/ControladorRelTurnosActualizar";
@@ -11,10 +11,10 @@ class RutaRelTurno{
 
     constructor(){
         this.rutaRelTurnoApi = Router();
-        this.rutaRelTurnoApi.post("/add", controladorRelTurnosCrear.llamarGrabarRelTurnos);
+        this.rutaRelTurnoApi.post("/add", datosRelTurnosCrear, validarDatos.ahora,controladorRelTurnosCrear.llamarGrabarRelTurnos);
         this.rutaRelTurnoApi.get("/getall", controladorRelTurnosConsulta.llamarObtenerTodos);
-        this.rutaRelTurnoApi.delete("/delete", controladorRelTurnosBorrar.llamarBorrar);
-        this.rutaRelTurnoApi.put("/update", controladorRelTurnosActualizar.llamarActualizarRelTurnos);
+        this.rutaRelTurnoApi.delete("/delete",datosRelTurnosBorrar ,controladorRelTurnosBorrar.llamarBorrar);
+        this.rutaRelTurnoApi.put("/update", datosRelTurnosActualizar, validarDatos.ahora,controladorRelTurnosActualizar.llamarActualizarRelTurnos);
     }
 
 }
---
+++ src/config/domain/ValidarRelTurnos.ts
@@ -0,0 +1,20 @@
+
+import { body, param } from "express-validator";
+
+export const datosRelTurnosCrear = [
+    body("cod_turnousuario", "Debe ser un número entero").isInt(),
+    body("cod_turno", "Debe ser un número entero").isInt(),
+    body("cod_usuario", "Debe ser un número entero").isInt(), 
+    body("cod_turnousuario", "Maximo 6 caracteres").isLength({ max: 6}),
+];
+
+export const datosRelTurnosBorrar = [
+    param("codRelTurnos", "Debe ser un numero").isInt(),
+    param("codRelTurnos", "Maximo 6 caracteres").isLength({ max: 6}),
+];
+
+export const datosRelTurnosActualizar = [
+    body("codRelTurnos", "Debe ser un número entero").isInt().not().isEmpty(),
+    body("cod_turnousuario", "Debe ser un número entero").isInt(),
+    body("cod_turno", "Debe ser un número entero").isInt(),
+];
